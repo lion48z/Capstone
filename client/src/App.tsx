928347b32e3aa941a15c5e6b82f31e94a2e64a05
lion48z <140863653+lion48z@users.noreply.github.com>
2024-02-14 18:23:39 -0600

    create frontend with TS redux
    clean up files and created files

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Farmergram</h1>
+    </div>
+  );
+}
+
+export default App;
