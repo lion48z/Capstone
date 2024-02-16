e1fb06edb92c56ccb9cd214db73063cf60f77234
lion48z <140863653+lion48z@users.noreply.github.com>
2024-02-15 19:20:59 -0600

    react router dom
    set up routes in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import { Counter } from './features/counter/Counter';
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage';
 
 
 function App() {
   return (
-    <div>
-      <h1>Farmergram</h1>
+    <div className='app'>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<LoginPage />}/>
+        <Route path ="/home" element={<HomePage />}/>
+        <Route path ="/profile/:userId" element={<ProfilePage />}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
