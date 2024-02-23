785c0e7f24ad15f6e4435ce66ccc7485772a11d6
lion48z <140863653+lion48z@users.noreply.github.com>
2024-02-23 11:25:15 -0600

    added a couple things
    need to focus but I am not there

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import { Counter } from './features/counter/Counter';
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage';
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-  return (
-    <div className='app'>
-      <BrowserRouter>
-      <Routes>
-        <Route path ="/" element={<LoginPage />}/>
-        <Route path ="/home" element={<HomePage />}/>
-        <Route path ="/profile/:userId" element={<ProfilePage />}/>
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
-      </Routes>
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
