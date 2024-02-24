d5d2cc421eb76ce6483afd668067692220c26d2b
lion48z <140863653+lion48z@users.noreply.github.com>
2024-02-24 15:49:49 -0600

    added a bunch of stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import HomePage from "../pages/homepage";
+import LoginPage from "../pages/loginpage";
+import ProfilePage from "..pages/profilepage.tsx";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
