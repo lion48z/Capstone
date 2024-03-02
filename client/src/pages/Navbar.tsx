69e6be066b0f4dc6b5570d9146be1602923250a7
lion48z <140863653+lion48z@users.noreply.github.com>
2024-03-02 15:18:19 -0600

    generic Navbar for now

--- client/src/pages/Navbar.tsx
+++ client/src/pages/Navbar.tsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
-
-
   return (
-    <h1>This is a NavBar</h1>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Farmergram
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/explore">
+          Explore
+        </Button>
+        <Button color="inherit" component={Link} to="/profile">
+          Profile
+        </Button>
+        <Button color="inherit" component={Link} to="/login">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
